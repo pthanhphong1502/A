5fdf9b5c275767ecec2fe79b4a151b8d750362db
Phong <pthanhphong@1502>
2024-05-02 16:19:54 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, Input, TemplateRef, ViewChild } from '@angular/core';
 import { TabComponent } from './tab/tab.component';
 import { Tab1Component } from './tab1/tab1.component';
 import { Tab2Component } from './tab2/tab2.component';
@@ -9,58 +9,77 @@ import { Tab3Component } from './tab3/tab3.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'tab';
-  activeTab: string = 'Personal Details';
-  activeDropdownTab: string = 'Personal Details';
+  activeTab: string = 'THÔNG TIN CÁ NHÂN';
+  activeDropdownTab: string = 'Sơ yếu lý lịch';
 
-  // Dữ liệu giả mạo
-  // tabsData = [
-  //   { name: 'Personal Details', component: TabComponent },
-  //   { name: 'Account Details', component: Tab1Component },
-  //   { name: 'Other Details', component: TabComponent }
-  // ];
+  @ViewChild('tab1Template') tab1Template!: TemplateRef<any>;
+  @ViewChild('tab2Template') tab2Template!: TemplateRef<any>;
 
-  lstTab = [
-    {
-      name: 'Sơ yếu lý lịch',
-      tabs: [
-        { name: 'Personal Details', component: TabComponent },
-        { name: 'Account Details', component: Tab1Component },
-        { name: 'Other Details', component: TabComponent }
-      ]
-    },
-    {
-      name: 'Pháp lý',
-      tabs: [
-        { name: 'Xyz', component: Tab2Component },
-        { name: 'Abc', component: Tab3Component },
-        { name: 'Oth Det', component: Tab2Component }
-      ]
-    }
-  ];
+  lstTab: any[] = [];
 
-  onTabClick(tab: any) {
-    this.activeTab = tab;
-    this.activeDropdownTab = tab;
+  constructor() {}
+
+
+  ngAfterViewInit(): void {
+    this.lstTab = [
+      {
+        name: 'Sơ yếu lý lịch',
+        tabs: [
+          { name: 'THÔNG TIN CÁ NHÂN', template: this.tab1Template },
+          { name: 'THÔNG TIN CÔNG VIỆC', template: this.tab2Template },
+          // { name: 'THÔNG TIN TUYỂN DỤNG', component: TabComponent },
+          // { name: 'THÂN NHÂN', component: Tab1Component },
+          // { name: 'THÔNG TIN ĐẢNG - ĐOÀN', component: TabComponent },
+          // { name: 'TIỂU SỬ CÁ NHÂN', component: Tab1Component },
+          // { name: 'LAO ĐỘNG NƯỚC NGOÀI', component: TabComponent },
+          // { name: 'HỒ SƠ CẦN NỘP', component: Tab1Component },
+        ]
+      },
+      {
+        name: 'Pháp lý',
+        tabs: [
+          { name: 'Tổng quan', template: this.tab1Template },
+          { name: 'Số bảo hiểm - Số lao động', template: this.tab2Template },
+          // { name: 'Tài khoản cá nhân', component: Tab2Component },
+          // { name: 'Hợp đồng lao động', component: Tab2Component },
+          // { name: 'Hộ chiếu', component: Tab3Component },
+          // { name: 'Thị thực', component: Tab2Component },
+          // { name: 'Giấy phép lao động', component: Tab2Component },
+          // { name: 'Miễn giấy phép lao động', component: Tab3Component },
+          // { name: 'Thẻ tạm trú', component: Tab2Component },
+          // { name: 'Đăng ký tạm trú', component: Tab2Component },
+          // { name: 'Giấy phép lái xe', component: Tab3Component },
+          // { name: 'Thông tin ủy quyền', component: Tab2Component },
+        ]
+      }
+    ];
   }
+
+  
+
 
   clickTab(item: any) {
     this.activeTab = item.tabs[0].name; // Mặc định chọn tab con đầu tiên
-    this.activeDropdownTab = item.tabs[0].name;
+    this.activeDropdownTab = item.name;
+    console.log(item.name);
+    console.log(item.tabs[0].name);
   }
 
   clickDropdownTab(tab: any) {
-    this.activeDropdownTab = tab;
+    // Gán giá trị của group.name cho activeDropdownTab
     // Tìm tab chứa component tương ứng với tab được chọn trong lstTab
     for (let group of this.lstTab) {
-      for (let t of group.tabs) {
-        if (t.name === tab) {
-          this.activeTab = t.name; // Cập nhật activeTab
-          return; // Kết thúc vòng lặp khi tìm thấy tab
+        for (let t of group.tabs) {
+            if (t.name === tab) {
+                this.activeTab = t.name;
+                console.log(t.name)
+                console.log(this.activeTab)
+                console.log(this.activeDropdownTab)
+                return; // Kết thúc vòng lặp khi tìm thấy tab
+            }
         }
-      }
     }
   }
-
 }
